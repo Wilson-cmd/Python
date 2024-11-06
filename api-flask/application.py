175102523951cf05472251ac5979608a3b1530a2
Wilson Costa <66930727+Wilson-cmd@users.noreply.github.com>
2024-11-06 17:04:38 -0300

    Update application.py

--- api-flask/application.py
+++ api-flask/application.py
@@ -4,9 +4,9 @@ from pdf2image import convert_from_bytes
 from io import BytesIO
 import requests
 import asyncio
-
-
-eventLoop = asyncio.new_event_loop()
+from jinja2 import Template
+import json
+import ast
 
 API_KEY = ""
 HEADERS = {"Content-Type": "application/json", "api-key": API_KEY,}
@@ -14,39 +14,72 @@ ENDPOINT = "https://<hub>.openai.azure.com/openai/deployments/<model-name>/chat/
 
 app = Flask(__name__)
 
+# CREATE REQUEST TO AZURE OPENAI
 async def requestsPostAPI(payload: object):
     return requests.post(ENDPOINT, headers=HEADERS, json=payload)
+
+#  PARSER TO JSON
+async def astLiteralEvalToJson(literalString: str) -> json:
+    return json.loads(json.dumps(ast.literal_eval(literalString)))
+
+# CREATE LIST'S BASE64
+async def createBase64FromList(document: list) -> list:
+    listDataBase64 = list()
+    for page in document:
+        with BytesIO() as BytesIOFork:
+            page.save(BytesIOFork, format = 'JPEG')
+            listDataBase64.append(b64encode(BytesIOFork.getvalue()).decode(encoding = 'utf-8'))
+            BytesIOFork.close()
+    return listDataBase64
+# CREATE TEMPLATE FOR CONTENT
+
+async def createContent(listDataBase64: list):
+    returnAsync = await astLiteralEvalToJson(Template(
+        '{%- set listToSetBase64 = [] -%} \
+        {%- macro content(listDataBase64, arr) -%} \
+            {%- for base64 in listDataBase64 -%} \
+                {{ arr.append({"type": "image_url", "image_url": {"url": "data:image/jpeg;base64,%s" % base64}})|replace("None", "") }} \
+            {%- endfor -%} \
+        {{listToSetBase64|list}} \
+        {%- endmacro -%} \
+        {{ content(listDataBase64, listToSetBase64) }} \
+    ').render(listDataBase64 = listDataBase64))
+    return returnAsync
+
+
+
+
+async def createPayloadWithFullMessage(*args):
+    returnAsync = await astLiteralEvalToJson(Template("{'messages': [{'role': 'system','content': '{{ prompt }}'}, \
+    {'role': 'user','content': {{imageURLWithContextType}}} \
+    ],'max_tokens': {{ maxToken }},'stream': {{ stream }}}").render(
+        maxToken = args[0] ,stream = args[1]
+        ,prompt = args[2] ,imageURLWithContextType = args[3]
+    ))
+    return returnAsync
+
+
 
 @app.post('/')
 def postDefault():
     parseToBytes = b64decode(request.data, validate = True)
     if parseToBytes.startswith(b'%PDF'):
         document = convert_from_bytes(b64decode(request.data), dpi = 450)
-        len(document) # COUNT PAGES: MAKE TEMPLATE
-        for page in document:
-            with BytesIO() as BytesIOFork:
-                page.save(BytesIOFork, format = 'JPEG')
-                data = b64encode(BytesIOFork.getvalue()).decode(encoding = 'utf-8')
-                BytesIOFork.close()
-                payload = {
-                    "messages": [
-                        {"role": "system","content": "Voce e uma ia que le imagens e descreve o conteudo EM PORTUGUES"},
-                        {"role": "user","content": [{"type":"image_url","image_url":{"url":f"data:image/jpeg;base64,{data}"}}]}
-                    ],
-                    "temperature": 0.0,
-                    "top_p": 0.95,
-                    "max_tokens": 800
-                }
-        response = eventLoop.run_until_complete(requestsPostAPI(payload))
+        
+        listDataBase64 = asyncio.run(createBase64FromList(document))
+        imageURLWithContextType = asyncio.run(createContent(listDataBase64))
+        
+        payload = asyncio.run(createPayloadWithFullMessage(
+        800
+        ,"False"
+        ,"Voce e uma ia que le imagens e descreve o conteudo EM PORTUGUES"
+        ,imageURLWithContextType))
+        
+        response = asyncio.run(requestsPostAPI(payload))
+        return response.json()           
 
-        return response.json()
-    else:
-        arrayStartWithBytes = [b'\x89PNG', b'\xff\xd8\xff\xe0', b'\xff\xd8\xff\xe1'] #Default bytes to check PNG JPEG JPG
-        for checkItem in arrayStartWithBytes:
-            if parseToBytes.startswith(checkItem):
-                return jsonify({"message": "waiting"})
-            else:
-                return jsonify({"message": "None"})
+
+
             
 if __name__ == '__main__':      
     app.run(host = '127.0.0.1', port = 3000, debug = True) 

256b95b064d8765f3f6237a36c9477ef43df1e39
Wilson Costa <66930727+Wilson-cmd@users.noreply.github.com>
2024-11-05 23:21:17 -0300

    Update application.py

--- api-flask/application.py
+++ api-flask/application.py
@@ -3,37 +3,50 @@ from base64 import b64decode, b64encode
 from pdf2image import convert_from_bytes
 from io import BytesIO
 import requests
-import time
+import asyncio
+
+
+eventLoop = asyncio.new_event_loop()
 
 API_KEY = ""
 HEADERS = {"Content-Type": "application/json", "api-key": API_KEY,}
-ENDPOINT = "https://<hub>.openai.azure.com/openai/deployments/<mode-name>/chat/completions?api-version=2024-02-15-preview"
+ENDPOINT = "https://<hub>.openai.azure.com/openai/deployments/<model-name>/chat/completions?api-version=2024-02-15-preview"
 
 app = Flask(__name__)
 
-def requestsPostAPI(payload):
-    time.sleep(10)
-    return requests.post(ENDPOINT, headers=HEADERS, json=payload).json()
+async def requestsPostAPI(payload: object):
+    return requests.post(ENDPOINT, headers=HEADERS, json=payload)
 
 @app.post('/')
 def postDefault():
-    document = convert_from_bytes(b64decode(request.data), dpi = 450)
-    for page in document:
-        with BytesIO() as BytesIOFork:
-            page.save(BytesIOFork, format = 'JPEG')
-            data = b64encode(BytesIOFork.getvalue()).decode(encoding = 'utf-8')
-            BytesIOFork.close()
-            payload = {
-                "messages": [
-                    {"role": "system","content": "Voce e uma ia que le imagens e descreve o conteudo EM PORTUGUES"},
-                    {"role": "user","content": [{"type":"image_url","image_url":{"url":f"data:image/jpeg;base64,{data}"}}]}
-             ],
-                "temperature": 0.7,
-                "top_p": 0.95,
-                "max_tokens": 800
+    parseToBytes = b64decode(request.data, validate = True)
+    if parseToBytes.startswith(b'%PDF'):
+        document = convert_from_bytes(b64decode(request.data), dpi = 450)
+        len(document) # COUNT PAGES: MAKE TEMPLATE
+        for page in document:
+            with BytesIO() as BytesIOFork:
+                page.save(BytesIOFork, format = 'JPEG')
+                data = b64encode(BytesIOFork.getvalue()).decode(encoding = 'utf-8')
+                BytesIOFork.close()
+                payload = {
+                    "messages": [
+                        {"role": "system","content": "Voce e uma ia que le imagens e descreve o conteudo EM PORTUGUES"},
+                        {"role": "user","content": [{"type":"image_url","image_url":{"url":f"data:image/jpeg;base64,{data}"}}]}
+                    ],
+                    "temperature": 0.0,
+                    "top_p": 0.95,
+                    "max_tokens": 800
                 }
-            response = requestsPostAPI(payload)
-        return response
+        response = eventLoop.run_until_complete(requestsPostAPI(payload))
+
+        return response.json()
+    else:
+        arrayStartWithBytes = [b'\x89PNG', b'\xff\xd8\xff\xe0', b'\xff\xd8\xff\xe1'] #Default bytes to check PNG JPEG JPG
+        for checkItem in arrayStartWithBytes:
+            if parseToBytes.startswith(checkItem):
+                return jsonify({"message": "waiting"})
+            else:
+                return jsonify({"message": "None"})
             
-        
-app.run(host = '127.0.0.1', port = 3000, debug = True) 
+if __name__ == '__main__':      
+    app.run(host = '127.0.0.1', port = 3000, debug = True) 

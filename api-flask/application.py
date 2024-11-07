d8691ec805e11b2b5a11049579b48d5a0396e57d
Wilson Costa <66930727+Wilson-cmd@users.noreply.github.com>
2024-11-07 14:41:15 -0300

    Update application.py

--- api-flask/application.py
+++ api-flask/application.py
@@ -1,20 +1,25 @@
 from flask import Flask, jsonify, request
 from base64 import b64decode, b64encode
 from pdf2image import convert_from_bytes
+from jinja2 import Template
 from io import BytesIO
 import requests
 import asyncio
-from jinja2 import Template
 import json
 import ast
+
+app = Flask(__name__)
 
 API_KEY = ""
 HEADERS = {"Content-Type": "application/json", "api-key": API_KEY,}
 ENDPOINT = "https://<hub>.openai.azure.com/openai/deployments/<model-name>/chat/completions?api-version=2024-02-15-preview"
+MaxToken = 1500
+Temperature = 0.0
+Stream = False
+Prompt = "Apos analisar o conteudo recebido, relate todas as informacoes que considerar relevante. Explique o porque considerou relevante a linha logica. Responda em pt-BR"
 
-app = Flask(__name__)
 
-# CREATE REQUEST TO AZURE OPENAI
+# CREATE REQUEST TO AZURE OPENAIMaxToken
 async def requestsPostAPI(payload: object):
     return requests.post(ENDPOINT, headers=HEADERS, json=payload)
 
@@ -31,8 +36,8 @@ async def createBase64FromList(document: list) -> list:
             listDataBase64.append(b64encode(BytesIOFork.getvalue()).decode(encoding = 'utf-8'))
             BytesIOFork.close()
     return listDataBase64
-# CREATE TEMPLATE FOR CONTENT
 
+# CREATE TEMPLATE FOR CONTENT
 async def createContent(listDataBase64: list):
     returnAsync = await astLiteralEvalToJson(Template(
         '{%- set listToSetBase64 = [] -%} \
@@ -46,20 +51,15 @@ async def createContent(listDataBase64: list):
     ').render(listDataBase64 = listDataBase64))
     return returnAsync
 
-
-
-
+# CREATEA PAYLOAD
 async def createPayloadWithFullMessage(*args):
     returnAsync = await astLiteralEvalToJson(Template("{'messages': [{'role': 'system','content': '{{ prompt }}'}, \
     {'role': 'user','content': {{imageURLWithContextType}}} \
-    ],'max_tokens': {{ maxToken }},'stream': {{ stream }}}").render(
-        maxToken = args[0] ,stream = args[1]
-        ,prompt = args[2] ,imageURLWithContextType = args[3]
+    ],'max_tokens': {{ maxToken }},'stream': {{ stream }}, 'temperature': {{temperature}}}").render(
+        maxToken = args[0] ,stream = args[1], prompt = args[2] ,imageURLWithContextType = args[3], temperature = args[4]
     ))
     return returnAsync
-
-
-
+arrayStartWithBytes = [b'\x89PNG', b'\xff\xd8\xff\xe0', b'\xff\xd8\xff\xe1']
 @app.post('/')
 def postDefault():
     parseToBytes = b64decode(request.data, validate = True)
@@ -69,17 +69,27 @@ def postDefault():
         listDataBase64 = asyncio.run(createBase64FromList(document))
         imageURLWithContextType = asyncio.run(createContent(listDataBase64))
         
-        payload = asyncio.run(createPayloadWithFullMessage(
-        800
-        ,"False"
-        ,"Voce e uma ia que le imagens e descreve o conteudo EM PORTUGUES"
-        ,imageURLWithContextType))
+        payload = asyncio.run(createPayloadWithFullMessage(MaxToken, Stream
+        ,Prompt
+        ,imageURLWithContextType, Temperature))
         
         response = asyncio.run(requestsPostAPI(payload))
-        return response.json()           
+        return response.json()
+    else:
+        for checkStartByte in arrayStartWithBytes:
+            if parseToBytes.startswith(checkStartByte):
+                imageURLWithContextType = asyncio.run(createContent([request.data.decode(encoding = 'utf-8')]))
+                payload = payload = asyncio.run(createPayloadWithFullMessage(MaxToken, Stream
+                ,Prompt
+                ,imageURLWithContextType, Temperature))
+                response = asyncio.run(requestsPostAPI(payload))
+                return response.json()
+            else:
+                return jsonify({"code": "401", "message": "check if your base64 match JPEG, PDF or PNG"})
+
 
 
 
             
 if __name__ == '__main__':      
-    app.run(host = '127.0.0.1', port = 3000, debug = True) 
+    app.run(host = '0.0.0.0', port = 3000, debug = True) 
